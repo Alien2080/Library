d5d5f2b12cdfaf1e1be6dadd62cfbf15a29676c6
Alien2080 <tom_aylen@hotmail.com>
2023-12-28 20:00:59 +1030

    formatting

--- src/script.js
+++ src/script.js
@@ -114,7 +114,7 @@ function closeAddBookModal() {
 function handleKeyboardInput(e) {
     if (e.key === 'Escape') {
         closeAddBookModal()
-    }   
+    }
 }
 
 function getBookFromInputForm() {
@@ -193,9 +193,9 @@ db.collection("users").add({
     last: "Lovelace",
     born: 1815
 })
-.then((docRef) => {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch((error) => {
-    console.error("Error adding document: ", error);
-});
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
